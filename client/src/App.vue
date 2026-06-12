e4de1f2ef3ce694377c5870f55c23c70eb4d611b
Stephen Ogwel <eusteve906@gmail.com>
2026-06-12 15:14:45 +0300

    chore: initial project setup. Install vuejs, tailwindcss and laravel

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-50 text-gray-900">
+   
+  </div>
+</template>
